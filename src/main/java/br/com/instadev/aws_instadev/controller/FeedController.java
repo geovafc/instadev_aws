462d3a7cfe9386c27ac2bd3a12ddb5d94c129a2d
Geovane Freitas <geovane.freitasc@gmail.com>
2021-05-28 19:26:59 -0300

    Adição da lib do spring actuator que irá criar um endpoint na AWS que irá nos informar se a aplicação está em execução ou não

--- src/main/java/br/com/instadev/aws_instadev/controller/FeedController.java
+++ src/main/java/br/com/instadev/aws_instadev/controller/FeedController.java
@@ -2,6 +2,9 @@ package br.com.instadev.aws_instadev.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedController {
 
     private static final Logger LOG = LoggerFactory.getLogger(FeedController.class);
+
+    @GetMapping("/{publicationId}")
+    public ResponseEntity<?> publication(@PathVariable Long publicationId ) {
+        LOG.info("Feed controller - publicationId: {}", publicationId);
+
+        return ResponseEntity.ok("PublicationId: " + publicationId);
+
+    }
 
 }
