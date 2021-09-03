fbc7f04cb3bcf7fef0be65361882748c00d16cd7
Geovane Freitas <geovane.freitasc@gmail.com>
2021-09-03 10:56:27 -0300

    Config topico sns

---
+++ src/main/java/br/com/instadev/aws_instadev/config/SnsConfig.java
@@ -0,0 +1,41 @@
+package br.com.instadev.aws_instadev.config;
+
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.Topic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SnsConfig {
+
+    @Value("{aws.region}")
+    private String awsRegion;
+
+    @Value("{aws.sns.topic.post.events.arn}")
+    private String postEventsTopic;
+
+
+//    Cliente responsável pela publicação da mensagem.
+    @Bean
+    public AmazonSNS snsClient(){
+            return AmazonSNSClientBuilder.standard()
+                    .withRegion(awsRegion)
+//                    Credencial pra poder acessar o tópico. Vai procurar no contexto onde nosso
+//                    serviço está executando credenciais que de permissão pra ele poder acessar
+//                    um tópico
+                    .withCredentials(new DefaultAWSCredentialsProviderChain())
+                    .build();
+    }
+
+//    Config do tópico que eu quero publicar
+    @Bean(name = "postEventsTopic")
+    public Topic snsPostEventsTopic(){
+        return new Topic().withTopicArn(postEventsTopic);
+    }
+
+
+}
